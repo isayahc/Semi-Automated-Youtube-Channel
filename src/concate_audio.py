4d384c02360dd45749ac087d4dcbaa52784c3497
isayahc <isayahculbertson@gmail.com>
2023-05-04 16:07:05 -0400

    clean up

--- src/concate_audio.py
+++ src/concate_audio.py
@@ -5,6 +5,13 @@ import natsort
 import random
 
 def combine_audio_files_directory(directory: str, output: str) -> AudioSegment:
+    """
+    Combines all .wav audio files in a given directory into a single audio file and exports it to the specified output file.
+    
+    :param directory: Path to the directory containing the audio files to be combined.
+    :param output: Path to the output file where the combined audio will be saved.
+    :return: The combined audio as a PyDub AudioSegment object.
+    """
     audio_files = [os.path.join(directory, f) for f in natsort.natsorted(os.listdir(directory)) if f.endswith('.wav')]
     combined_audio = AudioSegment.empty()
     for file in audio_files:
@@ -13,7 +20,15 @@ def combine_audio_files_directory(directory: str, output: str) -> AudioSegment:
     combined_audio.export(output, format='wav')
     return combined_audio
 
-def combine_audio_files_with_random_pause(directory, output) -> AudioSegment:
+def combine_audio_files_with_random_pause(directory:str, output:str) -> AudioSegment:
+    """
+    Combines all .wav audio files in a given directory into a single audio file with a random pause (300ms to 500ms) between
+    each file and exports it to the specified output file.
+    
+    :param directory: Path to the directory containing the audio files to be combined.
+    :param output: Path to the output file where the combined audio will be saved.
+    :return: The combined audio as a PyDub AudioSegment object.
+    """
     combined_audio = AudioSegment.empty()
     audio_files = [os.path.join(directory, f) for f in natsort.natsorted(os.listdir(directory)) if f.endswith('.wav')]
     for i, file in enumerate(audio_files):
--- src/create_video.py
+++ src/create_video.py
@@ -4,29 +4,34 @@ import subprocess
 from typing import Tuple
 from PIL import Image
 
-def create_webm_video(input_img: str, input_audio: str, output_file: str, output_extension: str = "mp4", img_size: Tuple[int, int] = (1080,1920)) -> None:
-    # Resize the input image to a resolution of 1920x1080
+def create_video(input_img: str, input_audio: str, output_file: str, output_extension: str = "mp4", img_size: Tuple[int, int] = (1080, 1920)) -> None:
+    """
+    Creates a video using an input image and audio, with the specified output extension and image size.
+    
+    :param input_img: Path to the input image file.
+    :param input_audio: Path to the input audio file.
+    :param output_file: Name of the output video file.
+    :param output_extension: Extension of the output video file. Default is 'mp4'.
+    :param img_size: Tuple containing the desired image width and height. Default is (1080, 1920).
+    """
+    # Resize the input image to the specified resolution
     image = Image.open(input_img)
     resized_image = image.resize(img_size)
     resized_image.save(input_img)
 
-    # Use the resized image and highest quality settings to create the video
-    output_extension = "mp4"
+    # Create the video using the resized image and specified output extension
     output_path = pathlib.Path(output_file)
     output_path = output_path.with_suffix(f".{output_extension}")
-    # command = ['ffmpeg', '-loop', '1', '-i', input_img, '-i', input_audio, '-c:v', 'libvpx-vp9', '-b:v', '0', '-crf', '0', '-c:a', 'libopus', '-strict -2', str(output_path), '-y']
-    command = ['ffmpeg', '-loop', '1', '-i', input_img, '-i', input_audio, "-vcodec", "mpeg4", "-acodec", "aac", '-shortest', str(output_path) , "-y","-r","2"]
+    command = ['ffmpeg', '-loop', '1', '-i', input_img, '-i', input_audio, "-vcodec", "mpeg4", "-acodec", "aac", '-shortest', str(output_path), "-y", "-r", "2"]
     print(command)
     subprocess.run(command)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Create a webm video using an input image and audio.")
+    parser = argparse.ArgumentParser(description="Create a video using an input image and audio.")
     parser.add_argument("input_img", help="Path to the input image")
     parser.add_argument("input_audio", help="Path to the input audio")
     parser.add_argument("output_file", help="Name of the output video file")
-    parser.add_argument("-e", "--output_extension", help="Extension of the output video file", default="webm")
-    # parser.add_argument("-s", "--img_size", help="Resolution of the output video (WIDTHxHEIGHT)", default="1920x1080")
+    parser.add_argument("-e", "--output_extension", help="Extension of the output video file", default="mp4")
 
     args = parser.parse_args()
-    # img_size = tuple(map(int, args.img_size.split("x")))
-    create_webm_video(args.input_img, args.input_audio, args.output_file, args.output_extension, )#img_size)
+    create_video(args.input_img, args.input_audio, args.output_file, args.output_extension)
