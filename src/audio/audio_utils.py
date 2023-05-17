9431e1d54e8e487589996080d6b3bc3b0de23ff2
isayahc <isayahculbertson@gmail.com>
2023-05-17 08:59:40 -0400

    fixed import inconsistancies

--- src/audio/audio_utils.py
+++ src/audio/audio_utils.py
@@ -7,7 +7,6 @@ from pydub import AudioSegment
 
 from src.audio import concate_audio
 from src.utils import (generate_subtitles, text_utils)
-from src.utils.generate_subtitles import *
 
 
 SWEAR_WORD_LIST_FILE_LOCATION = os.getenv('SWEAR_WORD_LIST_FILE_LOCATION_FILE_LOCATION')
@@ -34,7 +33,7 @@ def silence_segments(input_file, output_file, segments):
     audio.export(output_file, format="wav")
 
 def make_family_friendly(input_data:str,swear_word_list:List[str],output_data:str="output0.wav"):
-    x = transcribe_and_align(input_data)
+    x = generate_subtitles.transcribe_and_align(input_data)
     x_word_segments = x['word_segments']
 
     swear_word_segements = text_utils.filter_text_by_list(x_word_segments,swear_word_list)
