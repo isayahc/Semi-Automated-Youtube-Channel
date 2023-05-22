c2adc787fca1020d31419a14dbd157ba1a8ea296
isayahc <isayahculbertson@gmail.com>
2023-05-22 13:14:35 -0400

    added comments to upload_video.py

--- src/utils/upload_video.py
+++ src/utils/upload_video.py
@@ -5,24 +5,32 @@ from googleapiclient.discovery import build
 import argparse
 import os
 
-# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
-# the OAuth 2.0 information for this application, including its client_id and
-# client_secret.
+# This file contains the OAuth 2.0 information for this application, 
+# including its client_id and client_secret.
 CLIENT_SECRETS_FILE = "client_secret.json"
+# Define the scopes required by the API
 SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
 
+# This function handles OAuth2 authentication.
+# It returns an authenticated service that can be used to interact with the YouTube API.
 def get_authenticated_service():
+    # Create a flow object. This object holds the client_id and client_secret, 
+    # and is used for OAuth2 authentication.
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    # Authenticate and construct the service.
     credentials = flow.run_console()
     return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
+# This function initializes the video upload.
 def initialize_upload(youtube, options):
+    # Process the command line arguments.
     tags = None
     if options.keywords:
         tags = options.keywords.split(',')
 
+    # Construct the body of the request.
     body=dict(
         snippet=dict(
             title=options.title,
@@ -35,34 +43,36 @@ def initialize_upload(youtube, options):
         )
     )
 
+    # Call the YouTube API's videos.insert method to create and upload the video.
     insert_request = youtube.videos().insert(
         part=",".join(body.keys()),
         body=body,
         media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True)
     )
 
+    # Execute the upload.
     resumable_upload(insert_request, options)
 
-
+# This function uploads a file and tracks its progress.
 def resumable_upload(request, options):
     response = None
     while response is None:
         status, response = request.next_chunk()
+        # If the upload is successful, set the video's thumbnail.
         if 'id' in response:
             print(f"Video id {response['id']} was successfully uploaded.")
             set_thumbnail(options, response['id'])
 
-            
-
+# This function sets the thumbnail of the video after it has been uploaded.
 def set_thumbnail(options, video_id):
     youtube.thumbnails().set(
         videoId=video_id,
         media_body=MediaFileUpload(options.thumbnail)
     ).execute()
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    # Add arguments for command line options.
     parser.add_argument('--file', required=True, help='Video file to upload')
     parser.add_argument('--title', help='Video title', default='Test Title')
     parser.add_argument('--description', help='Video description', default='Test Description')
@@ -72,6 +82,7 @@ if __name__ == '__main__':
     parser.add_argument('--thumbnail', help='Thumbnail image file', default='')
     args = parser.parse_args()
 
+    # Authenticate and initialize the service.
     youtube = get_authenticated_service()
     try:
         initialize_upload(youtube, args)
