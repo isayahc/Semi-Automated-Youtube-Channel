3d8cab12d70b7455f1a8f355a15e8326d173660a
isayahc <isayahculbertson@gmail.com>
2023-07-05 16:07:33 -0400

    added to tag_examples.py and updated requirements.txt

--- examples/tags_example.py
+++ examples/tags_example.py
@@ -11,7 +11,12 @@ data = [
 "https://www.youtube.com/watch?v=Af3ZG47oT7I",
 "https://www.youtube.com/watch?v=m1sgtzgrhwY",
 "https://www.youtube.com/watch?v=rn8oef21Igg",
-"https://www.youtube.com/watch?v=G88XPaeGYFo",]
+"https://www.youtube.com/watch?v=G88XPaeGYFo",
+"https://www.youtube.com/watch?v=CPCKdlU5cg4",
+"https://www.youtube.com/watch?v=OW1EmG7b4DU",
+"https://www.youtube.com/watch?v=muuhgrige5Q",
+"https://www.youtube.com/watch?v=f-sRcVkZ9yg"
+]
 
 # tag_list = get_youtube_video_tags.get_video_tags(youtube_url)
 
